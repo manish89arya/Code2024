68d6295728bd2dc7920559f7f56111c48aa5234f
Manish Arya <manish89arya@gmail.com>
2025-02-04 00:42:10 +0530

    reverse a string

--- src/main/java/Programs.java
+++ src/main/java/Programs.java
@@ -3,5 +3,9 @@ public class Programs {
     public static void main(String[] args)
     {
         System.out.println( "Code 2025 - Back to basics !");
+
+        String input = "Selenium";
+
+        StringPrograms.reverseString(input);
     }
 }
--- src/main/java/StringPrograms.java
+++ src/main/java/StringPrograms.java
@@ -1,6 +1,15 @@
 public class StringPrograms {
     public static void reverseString(String str) {
 
+    int length = str.length(); // 8
+        System.out.println("Length of String is: "+length);
+        System.out.println("First char of String is: "+str.charAt(length-1));
+    String rev = "";
+    for(int i=length-1; i>=0; i--)
+    {
+        rev = rev+str.charAt(i);
+    }
+        System.out.println(rev);
     }
 
     public static void frequentlyRepeatingChar(String str) {
